79e33212c2138663616b37c4fe711189a12b97ad
rathik <rathik@email.chop.edu>
2019-08-08 14:55:09 -0400

    update splice variant filtering script

--- R/filter_splice_variants.R
+++ R/filter_splice_variants.R
@@ -1,6 +1,6 @@
 # Author: Komal S. Rathi
 # Date: 12/23/2018
-# Function: Filter and summarize disruptive splice variants 
+# Function: Filter and summarize disruptive splice variants
 # inspired from Cummings et al.
 
 setwd('~/Projects/DGD_Mendelian_RNASeq/')
@@ -22,15 +22,16 @@ total <- rbind(total,only1sample)
 
 ############################################
 # Method 2: Events seen in many individuals, but only seen in one individual with read support higher than say, 10 reads (we can make it more stringent i.e. 20 reads)
-# filter < 5 reads (n = 9) 
+# filter < 5 reads (n = 9)
 filtered.reads.5 <- read.delim('results/splice-events-patient/FilteredJunctions_ReadSupport10_FilterReads5.txt')
 filtered.reads.5 <- filtered.reads.5[which(filtered.reads.5$NSamplesSeen == 1),]
 filtered.reads.5 <- filtered.reads.5[which(filtered.reads.5$NTimesSeen >= 10),]
 filtered.reads.5 <- filtered.reads.5[-which(filtered.reads.5$Pos %in% only1sample$Pos),]
 
-# n = 3 
+# n = 3
 total <- rbind(total, filtered.reads.5)
-total <- unique(total[,c('NSeen.Samples','Gene','Pos')])
-colnames(total)[1] <- 'Samples'
+total$PropSeen.Samples <- gsub(':.*','',total$PropSeen.Samples)
+total$PropSeen.Samples <- as.numeric(total$PropSeen.Samples)*100
+total <- unique(total[,c('Sample','Gene','Pos','Annotation','NTimesSeen','NSamplesSeen','PropSeen.Samples')])
 total <- unique(total)
 write.table(total, file = 'results/splice-events-summary/splice-events-rnaseq.txt', quote = F, sep = "\t", row.names = F)
